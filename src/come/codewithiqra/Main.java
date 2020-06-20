3ec6fca01904d48e49c0f4190a9d4a26dca9c660
HP 6th <iqra615>
2020-06-20 10:54:39 -0700

    Computes Value

---
+++ src/come/codewithiqra/Main.java
@@ -0,0 +1,16 @@
+package come.codewithiqra;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int n;
+	char s1,s2,s3;
+        Scanner in= new Scanner(System.in);
+        System.out.print("Input any number: ");
+        n= in.nextInt();
+        System.out.printf("%d + %d%d + %d%d%d\n", n, n, n, n, n, n);
+
+    }
+}
